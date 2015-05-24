47bc650efdc3c9635e60818b3620f286d38f862e
Linda <linda@brain.do>
2015-05-23 22:52:22 -0400

    run_analysis

---
+++ run_analysis.R
@@ -0,0 +1,54 @@
+# 1. 
+
+tmp1 <- read.table("UCI HAR Dataset/train/X_train.txt")
+tmp2 <- read.table("UCI HAR Dataset/test/X_test.txt")
+X <- rbind(tmp1, tmp2)
+
+tmp1 <- read.table("UCI HAR Dataset/train/subject_train.txt")
+tmp2 <- read.table("UCI HAR Dataset/test/subject_test.txt")
+S <- rbind(tmp1, tmp2)
+
+tmp1 <- read.table("UCI HAR Dataset/train/y_train.txt")
+tmp2 <- read.table("UCI HAR Dataset/test/y_test.txt")
+Y <- rbind(tmp1, tmp2)
+
+# 2. 
+
+features <- read.table("UCI HAR Dataset/features.txt")
+indices_of_good_features <- grep("-mean\\(\\)|-std\\(\\)", features[, 2])
+X <- X[, indices_of_good_features]
+names(X) <- features[indices_of_good_features, 2]
+names(X) <- gsub("\\(|\\)", "", names(X))
+names(X) <- tolower(names(X))
+
+# 3. 
+
+activities <- read.table("UCI HAR Dataset/activity_labels.txt")
+activities[, 2] = gsub("_", "", tolower(as.character(activities[, 2])))
+Y[,1] = activities[Y[,1], 2]
+names(Y) <- "activity"
+
+# 4. 
+
+names(S) <- "subject"
+cleaned <- cbind(S, Y, X)
+write.table(cleaned, "UCI HAR Dataset/merged_clean_data.txt")
+
+# 5.
+uniqueSubjects = unique(S)[,1]
+numSubjects = length(unique(S)[,1])
+numActivities = length(activities[,1])
+numCols = dim(cleaned)[2]
+result = cleaned[1:(numSubjects*numActivities), ]
+
+row = 1
+for (s in 1:numSubjects) {
+  for (a in 1:numActivities) {
+    result[row, 1] = uniqueSubjects[s]
+    result[row, 2] = activities[a, 2]
+    tmp <- cleaned[cleaned$subject==s & cleaned$activity==activities[a, 2], ]
+    result[row, 3:numCols] <- colMeans(tmp[, 3:numCols])
+    row = row+1
+  }
+}
+write.table(result, "data_set_with_the_averages.txt")
